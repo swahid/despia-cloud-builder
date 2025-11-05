e54bbdd8e7db2345d70c10e18a9f2593eacfe3af
Saurav Wahid <saurav@metatude.com>
2025-11-05 17:24:05 +0600

    set project id

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,12 +1,12 @@
 # Build image locally
-docker build -t gcr.io/YOUR_PROJECT_ID/despia-cloud-builder ./api
+docker build -t gcr.io/despia-cloud/despia-cloud-builder ./api
 
 # Push image to GCP
-docker push gcr.io/YOUR_PROJECT_ID/despia-cloud-builder
+docker push gcr.io/despia-cloud/despia-cloud-builder
 
 # Deploy Cloud Run service
 gcloud run deploy despia-cloud-builder \
-  --image gcr.io/YOUR_PROJECT_ID/despia-cloud-builder \
+  --image gcr.io/despia-cloud/despia-cloud-builder \
   --platform managed \
   --region us-central1 \
   --allow-unauthenticated \
